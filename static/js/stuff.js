e0e99a38d9c5842d71f991df4655d179d1fb5c2d
Matt Hong <mr.hongaku@berkeley.edu>
2014-02-25 21:25:29 -0800

    Lab 2 finished!

--- static/js/stuff.js
+++ static/js/stuff.js
@@ -1,4 +1,4 @@
-alert('Hello World'); // edit me!
+alert('Some Stuff'); // edit me!
 
 // Problem 1 (Say Hello!) ---------------------------------------------------
 $('#say_hello').click(function() {
@@ -8,17 +8,29 @@ $('#say_hello').click(function() {
 
 // Problem 2 (Houdini) ------------------------------------------------------
 $('#disappear').click(function() {
-  //WRITE CODE HERE
+	$(houdini_text).hide();
 });
 
 $('#reappear').click(function() {
-  //WRITE CODE HERE
+  	$(houdini_text).show();
 });
 
 
 // Problem 3 (Tickle Me Pink) -----------------------------------------------
 // WRITE CODE HERE
+$('#pink').click(function() {
+	$(tickled_text).css('color', 'pink')
+});
 
+$('#depink').click(function() {
+	$(tickled_text).css('color', 'black')
+});
 
 // Problem 4 (Greet Me) -----------------------------------------------------
 // WRITE CODE HERE
+$('#greet').click(function() {
+	var rows = $('#my_name').my_name()
+	alert('my_name')
+
+
+});
