772c6ecaf30e324905ce1ab092a30e9b8984793f
Matt Hong <mr.hongaku@berkeley.edu>
2014-02-26 18:29:33 -0800

    added imgrr

---
+++ static/js/imgrr.js
@@ -0,0 +1,23 @@
+// Problem 2 (Peekaboo) ------------------------------------------------------
+
+$('#toggle_img').click(function() {
+if $(toggle_img).text(toggle_img === 'Go Away!') {
+	$(main_img).hide() ;
+	$(toggle_img).text('Come Back!') ;
+} else if (toggle_img).text( === 'Come Back!') {
+	$(main_img).show() ;
+	$(toggle_img).text('Go Away!') ;
+}
+});
+// WRITE CODE HERE
+
+// Problem 3 (Swap Em) -----------------------------------------------
+// WRITE CODE HERE
+
+// Problem 4 (Find the Source) -------------------------------------------------
+$('.clickable').click(function() {
+  // WRITE CODE HERE
+});
+
+// Problem 5 (Imgrr) -------------------------------------------------
+// WRITE CODE HERE
