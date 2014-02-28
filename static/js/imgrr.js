e0ed8890827b94e10ebe15fdc30d415a88509623
Matt Hong <mr.hongaku@berkeley.edu>
2014-02-27 21:48:56 -0800

    finished lab 3

--- static/js/imgrr.js
+++ static/js/imgrr.js
@@ -1,10 +1,10 @@
 // Problem 2 (Peekaboo) ------------------------------------------------------
 
 $('#toggle_img').click(function() {
-if $(toggle_img).text(toggle_img === 'Go Away!') {
+if ($(toggle_img).text() === 'Go Away!') {
 	$(main_img).hide() ;
 	$(toggle_img).text('Come Back!') ;
-} else if (toggle_img).text( === 'Come Back!') {
+} else if ($(toggle_img).text() === 'Come Back!') {
 	$(main_img).show() ;
 	$(toggle_img).text('Go Away!') ;
 }
@@ -13,11 +13,30 @@ if $(toggle_img).text(toggle_img === 'Go Away!') {
 
 // Problem 3 (Swap Em) -----------------------------------------------
 // WRITE CODE HERE
+$('#change_img').click(function() {
+if ($(new_img_file).val() === 'giraffe.jpg') {
+	$(main_img).attr('src', '/static/img/giraffe.jpg')
+} if ($(new_img_file).val() === 'red_panda.jpg') {
+	$(main_img).attr('src', '/static/img/red_panda.jpg')
+} if ($(new_img_file).val() === 'gorilla.jpg') {
+	$(main_img).attr('src', '/static/img/gorilla.jpg')
+}
 
+});
 // Problem 4 (Find the Source) -------------------------------------------------
 $('.clickable').click(function() {
-  // WRITE CODE HERE
+	$(this).attr('width', '200px')
+	alert($('.clickable').attr('src')); 
 });
 
 // Problem 5 (Imgrr) -------------------------------------------------
 // WRITE CODE HERE
+$('.small').click(function() {
+if ($(this).attr('src') === '/static/img/giraffe.jpg') {
+	$(imgrr).attr('src', '/static/img/giraffe.jpg')
+} if ($(this).attr('src') === '/static/img/red_panda.jpg') {
+	$(imgrr).attr('src', '/static/img/red_panda.jpg')
+} if ($(this).attr('src') === '/static/img/gorilla.jpg') {
+	$(imgrr).attr('src', '/static/img/gorilla.jpg')
+}
+});
