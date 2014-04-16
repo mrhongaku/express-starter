47263d54ccad9f0f82d110f30edbc7a80d75d0b5
Matt Hong <mr.hongaku@berkeley.edu>
2014-04-15 19:18:18 -0700

    animated reactions

--- static/js/chain_reaction.js
+++ static/js/chain_reaction.js
@@ -4,9 +4,11 @@ $(document).ready(function() {
   var context = canvas.getContext('2d');
   var width = canvas.width;
   var height = canvas.height;
-  var numBalls = 1;
+  var numBalls = 10;
   var balls = [];
   var color = ['black', 'blue', 'pink', 'green', 'orange'];
+  var reactions = [];
+  var randomcolor = color[Math.floor(Math.random()*color.length)];
   for (var i = 0; i < numBalls; i++) {
 
     var Circle = {xCoor: canvas.width*Math.random(), yCoor: canvas.height*Math.random(), radius: 50*Math.random(), xv: 5*Math.random(), yv: 5*Math.random()};
@@ -17,9 +19,8 @@ $(document).ready(function() {
     // Find the mouse x and y relative to the top-left corner of the canvas
     var x = e.pageX - $(this).offset().left;
     var y = e.pageY - $(this).offset().top;
-    var randomcolor = color[Math.floor(Math.random()*color.length)];
-    var new_ball = {xCoor: x, yCoor: y, radius: 20 * Math.random(), xv: 20 * Math.random(), yv: 20 * Math.random()};
-    balls.push(new_ball);
+    var new_ball = {xCoor: x, yCoor: y, radius: 1}
+    reactions.push(new_ball);
     });
 
   // Run an interation of the game
@@ -38,9 +39,25 @@ $(document).ready(function() {
       context.closePath();
       context.stroke(context.strokeStyle = randomcolor);
     }
+    for (var i = 0; i < reactions.length; i++) {
+      if (reactions[i].radius <= 30) {
+      reactions[i].radius += reactions[i].radius+1 
+      }
+    }
+
+    for (var i = 0; i < reactions.length; i++) {
+      context.beginPath();
+      context.arc(reactions[i].xCoor, reactions[i].yCoor, reactions[i].radius, 0, 2*Math.PI);
+      context.closePath();
+      context.fillStyle = 'red';
+      context.fill();
+      context.stroke(context.strokeStyle = 'red');
+    }
+
 
     requestAnimationFrame(updateGame);
   };
+
 
   // Handle a canvas click event
 
